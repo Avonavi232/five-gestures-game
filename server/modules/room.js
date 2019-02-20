55fbb5171a91228bc92e0ddc96a8c1ca733ec73e
Aleksandr Ivanov <avonavi232@gmail.com>
2019-02-20 10:49:42 +0300

    Room class updated

--- server/modules/room.js
+++ server/modules/room.js
@@ -1,8 +1,30 @@
 const uuid = require('uuid');
 
 class Room {
-	constructor(){
+	constructor(io, settings){
 		this.roomID = uuid.v4();
+		this.players = new Set();
+		this.io = io;
+		this.settings = settings;
+	}
+
+	addPlayer(player){
+		this.players.add(player);
+		player._joinRoom(this.roomID);
+	}
+
+	deletePlayer(playerID){
+		const found = Array
+			.from(this.players)
+			.find(player => player.playerID === playerID);
+		if (found) {
+			return this.players.delete(found);
+		}
+		return false;
+	}
+
+	broadcast(event, ...args){
+		this.io.to(this.roomID).emit(event, ...args);
 	}
 }
 
